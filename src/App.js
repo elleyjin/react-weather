94dfa9e33736bcb01aea4d426e6f266bbf7c5aef
elleyjin <95072132+elleyjin@users.noreply.github.com>
2024-09-20 00:56:28 +0800

    initialize components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Navbar";
+import Header from "./Header";
+import Forecast from "./Forecast";
+import Footer from "./Footer";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar />
+        <Header />
+        <Forecast />
+        <Footer />
       </header>
     </div>
   );
