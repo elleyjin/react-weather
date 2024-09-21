df611d9bb196b5a51c54044f3e953343e4fd26a0
elleyjin <95072132+elleyjin@users.noreply.github.com>
2024-09-21 17:47:42 +0800

    added bootstrap

--- src/Header.js
+++ src/Header.js
@@ -3,37 +3,39 @@ import "./Header.css";
 
 export default function Header() {
   return (
-    <div className="main-weather-container">
-      <img
-        id="icon"
-        src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/097/811/original/sun.png?1695302792"
-        alt=""
-        className="main-weather-img"
-      ></img>
-      <div>
-        <span className="current-temp" id="temperature">
-          27
-        </span>
-        <span className="units">
-          {/* <a href="#" id="celsius-link" className="active"> */}
-          °C
-          {/* </a>{" "} */}| {/* <a href="" id="farenheit-link"> */}
-          °F
-          {/* </a> */}
-        </span>
+    <div className="Header">
+      <div className="main-weather-container">
+        <img
+          id="icon"
+          src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/097/811/original/sun.png?1695302792"
+          alt=""
+          className="main-weather-img"
+        ></img>
+        <div>
+          <span className="current-temp" id="temperature">
+            27
+          </span>
+          <span className="units">
+            {/* <a href="#" id="celsius-link" className="active"> */}
+            °C
+            {/* </a>{" "} */}| {/* <a href="" id="farenheit-link"> */}
+            °F
+            {/* </a> */}
+          </span>
+        </div>
+        <p className="weather-description" id="description"></p>
+        <p className="today-temp">
+          <strong>
+            {" "}
+            l: <span id="low-temp"></span> h: <span id="high-temp"></span>
+          </strong>
+        </p>
+        <p>
+          <span id="date"></span>
+          <span id="time"></span>
+          <span id="suffix"></span>
+        </p>
       </div>
-      <p className="weather-description" id="description"></p>
-      <p className="today-temp">
-        <strong>
-          {" "}
-          l: <span id="low-temp"></span> h: <span id="high-temp"></span>
-        </strong>
-      </p>
-      <p>
-        <span id="date"></span>
-        <span id="time"></span>
-        <span id="suffix"></span>
-      </p>
     </div>
   );
 }
